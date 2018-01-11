fd777d0e33ae6934a0c060dbe881439601be2d6b
binaryFate <binaryfate@users.noreply.github.com>
2018-01-11 19:54:18 +0100

    Add Bech32 support + boolean paramm for testnet, no magicbytes

--- bitcoinaddress/validation.py
+++ bitcoinaddress/validation.py
@@ -8,6 +8,8 @@ import string
 from hashlib import sha256
 
 digits58 = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+CHARSET = "qpzry9x8gf2tvdw0s3jn54khce6mua7l" # BECH32 charset
+
 
 def _bytes_to_long(bytestring, byteorder):
     """Convert a bytestring to a long
@@ -75,7 +77,7 @@ def encode_base58(bytestring):
         (n, rest) = divmod(n, 58)
     return zeros * '1' + result[::-1]  # reverse string
 
-def validate(bitcoin_address, magicbyte=0):
+def validate(bitcoin_address, testnet=False):
     """Check the integrity of a bitcoin address
 
     Returns False if the address is invalid.
@@ -84,12 +86,22 @@ def validate(bitcoin_address, magicbyte=0):
     >>> validate('')
     False
     """
-    if isinstance(magicbyte, int):
-        magicbyte = (magicbyte,)
+    if testnet:
+        magicbyte=(111,)
+        bech32_hrp = "tb"
+    else:
+        magicbyte=(0, 5)
+        bech32_hrp = "bc"
+
+    if (bitcoin_address[:2]).lower() == bech32_hrp:
+        return validate_bech32(bitcoin_address, bech32_hrp)
+    else:
+        return validate_base58(bitcoin_address, magicbyte)
+
+def validate_base58(bitcoin_address, magicbyte):
     clen = len(bitcoin_address)
-    if clen < 27 or clen > 35: # XXX or 34?
+    if clen < 27 or clen > 35:
         return False
-    allowed_first = tuple(string.digits)
     try:
         bcbytes = decode_base58(bitcoin_address, 25)
     except ValueError:
@@ -108,3 +120,84 @@ def validate(bitcoin_address, magicbyte=0):
     # for example '14oLvT2' has a valid checksum, but is not a valid btc
     # address
     return bitcoin_address == encode_base58(bcbytes)
+
+
+# All BECH32 code below is from Pieter Wuille's reference implementation associated with BIP173
+# See https://github.com/sipa/bech32/blob/master/ref/python/segwit_addr.py
+# and https://github.com/bitcoin/bips/blob/master/bip-0173.mediawiki#Reference_implementations
+def validate_bech32(bitcoin_address, hrp):
+    return (decode(hrp, bitcoin_address)[0] is not None)
+
+def bech32_polymod(values):
+    """Internal function that computes the Bech32 checksum."""
+    generator = [0x3b6a57b2, 0x26508e6d, 0x1ea119fa, 0x3d4233dd, 0x2a1462b3]
+    chk = 1
+    for value in values:
+        top = chk >> 25
+        chk = (chk & 0x1ffffff) << 5 ^ value
+        for i in range(5):
+            chk ^= generator[i] if ((top >> i) & 1) else 0
+    return chk
+
+
+def bech32_hrp_expand(hrp):
+    """Expand the HRP into values for checksum computation."""
+    return [ord(x) >> 5 for x in hrp] + [0] + [ord(x) & 31 for x in hrp]
+
+
+def bech32_verify_checksum(hrp, data):
+    """Verify a checksum given HRP and converted data characters."""
+    return bech32_polymod(bech32_hrp_expand(hrp) + data) == 1
+
+def bech32_decode(bech):
+    """Validate a Bech32 string, and determine HRP and data."""
+    if ((any(ord(x) < 33 or ord(x) > 126 for x in bech)) or
+            (bech.lower() != bech and bech.upper() != bech)):
+        return (None, None)
+    bech = bech.lower()
+    pos = bech.rfind('1')
+    if pos < 1 or pos + 7 > len(bech) or len(bech) > 90:
+        return (None, None)
+    if not all(x in CHARSET for x in bech[pos+1:]):
+        return (None, None)
+    hrp = bech[:pos]
+    data = [CHARSET.find(x) for x in bech[pos+1:]]
+    if not bech32_verify_checksum(hrp, data):
+        return (None, None)
+    return (hrp, data[:-6])
+
+def convertbits(data, frombits, tobits, pad=True):
+    """General power-of-2 base conversion."""
+    acc = 0
+    bits = 0
+    ret = []
+    maxv = (1 << tobits) - 1
+    max_acc = (1 << (frombits + tobits - 1)) - 1
+    for value in data:
+        if value < 0 or (value >> frombits):
+            return None
+        acc = ((acc << frombits) | value) & max_acc
+        bits += frombits
+        while bits >= tobits:
+            bits -= tobits
+            ret.append((acc >> bits) & maxv)
+    if pad:
+        if bits:
+            ret.append((acc << (tobits - bits)) & maxv)
+    elif bits >= frombits or ((acc << (tobits - bits)) & maxv):
+        return None
+    return ret
+
+def decode(hrp, addr):
+    """Decode a segwit address."""
+    hrpgot, data = bech32_decode(addr)
+    if hrpgot != hrp:
+        return (None, None)
+    decoded = convertbits(data[1:], 5, 8, False)
+    if decoded is None or len(decoded) < 2 or len(decoded) > 40:
+        return (None, None)
+    if data[0] > 16:
+        return (None, None)
+    if data[0] == 0 and len(decoded) != 20 and len(decoded) != 32:
+        return (None, None)
+    return (data[0], decoded)
--- tests.py
+++ tests.py
@@ -107,17 +107,47 @@ class TestValidMagicbytes(TestCase):
                 'mpc1rKeaMSCuQnJevMViLuq8uWjHwgdjiV'))
         self.assertTrue(
             bitcoinaddress.validate(
-                'mpc1rKeaMSCuQnJevMViLuq8uWjHwgdjiV', magicbyte=111))
+                'mpc1rKeaMSCuQnJevMViLuq8uWjHwgdjiV', testnet=True))
 
     def test_valid_multisig(self):
         self.assertTrue(
             bitcoinaddress.validate(
-                '3QJmV3qfvL9SuYo34YihAf3sRCW3qSinyC', magicbyte=5))
+                '3QJmV3qfvL9SuYo34YihAf3sRCW3qSinyC', testnet=False))
 
     def test_valid_multiple_magicbytes(self):
         self.assertTrue(
             bitcoinaddress.validate(
-                'mpc1rKeaMSCuQnJevMViLuq8uWjHwgdjiV', magicbyte=(0, 111)))
+                'mpc1rKeaMSCuQnJevMViLuq8uWjHwgdjiV', testnet=True))
+
+class TestValidBECH32(TestCase):
+    def test_valid_and_invalid_bech32(self):
+        # Address, testnet
+        VALID_ADDRESS = [
+            ["BC1QW508D6QEJXTDG4Y5R3ZARVARY0C5XW7KV8F3T4", 0],
+            ["tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3q0sl5k7", 1],
+            ["bc1pw508d6qejxtdg4y5r3zarvary0c5xw7kw508d6qejxtdg4y5r3zarvary0c5xw7k7grplx", 0],
+            ["BC1SW50QA3JX3S", 0],
+            ["tb1qqqqqp399et2xygdj5xreqhjjvcmzhxw4aywxecjdzew6hylgvsesrxh6hy", 1],
+        ]
+
+        INVALID_ADDRESS = [
+            ["tc1qw508d6qejxtdg4y5r3zarvary0c5xw7kg3g4ty", 1],
+            ["bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kv8f3t5", 0],
+            ["BC13W508D6QEJXTDG4Y5R3ZARVARY0C5XW7KN40WF2", 0],
+            ["bc1rw5uspcuh", 0],
+            ["bc10w508d6qejxtdg4y5r3zarvary0c5xw7kw508d6qejxtdg4y5r3zarvary0c5xw7kw5rljs90", 0],
+            ["BC1QR508D6QEJXTDG4Y5R3ZARVARYV98GJ9P", 0],
+            ["tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3q0sL5k7", 1],
+            ["bc1zw508d6qejxtdg4y5r3zarvaryvqyzf3du", 0],
+            ["tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3pjxtptv", 1],
+            ["bc1gmk9yu", 0],
+        ]
+
+        for add, testnet in VALID_ADDRESS:
+            self.assertTrue(bitcoinaddress.validate(add, testnet))
+
+        for add, testnet in INVALID_ADDRESS:
+            self.assertFalse(bitcoinaddress.validate(add, testnet))
 
 if __name__ == '__main__':
     from unittest import main
